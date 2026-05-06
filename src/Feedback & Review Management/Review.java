7316af8f70e45ee1e247af84b6a2805fa400c518
IT25103685 <it25103685@my.sliit.lk>
2026-05-06 14:48:08 +0530

    Update Review.java

--- src/Feedback & Review Management/Review.java
+++ src/Feedback & Review Management/Review.java
@@ -5,13 +5,6 @@ public abstract class Review {
     private String reviewerId;
     private int rating;
     private String comment;
-
-    public Review(String reviewId, String propertyId, String reviewerId, int rating, String comment) {
-        this.reviewId = reviewId;
-        this.propertyId = propertyId;
-        this.reviewerId = reviewerId;
-        this.rating = rating;
-        this.comment = comment;
-    }
+    
 
 }
