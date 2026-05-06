2733110ff2d890afebbb92ed3e83f373725ae0e4
IT25103685 <it25103685@my.sliit.lk>
2026-05-06 14:42:01 +0530

    add private fields to Review

--- src/Feedback & Review Management/Review.java
+++ src/Feedback & Review Management/Review.java
@@ -1,3 +1,11 @@
 public abstract class Review {
 
+    private String reviewId;
+    private String propertyId;
+    private String reviewerId;
+    private int rating;
+    private String comment;
+
+
+
 }
