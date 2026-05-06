b65ad4129d4158435b4bb4a960603308e4601b5b
IT25103685 <it25103685@my.sliit.lk>
2026-05-06 14:55:06 +0530

    add constructor to Review

--- src/Feedback & Review Management/Review.java
+++ src/Feedback & Review Management/Review.java
@@ -5,6 +5,13 @@ public abstract class Review {
     private String reviewerId;
     private int rating;
     private String comment;
-    
+
+    public Review(String reviewId, String propertyId, String reviewerId, int rating, String comment) {
+        this.reviewId = reviewId;
+        this.propertyId = propertyId;
+        this.reviewerId = reviewerId;
+        this.rating = rating;
+        this.comment = comment;
+    }
 
 }
