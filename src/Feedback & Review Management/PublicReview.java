ef0e61164f272330410de1d101953c78e40c098d
IT25103685 <it25103685@my.sliit.lk>
2026-05-07 12:27:14 +0530

    add constructor to PublicReview

--- src/Feedback & Review Management/PublicReview.java
+++ src/Feedback & Review Management/PublicReview.java
@@ -1,5 +1,7 @@
 public class PublicReview extends Review {
 
-    
+    public PublicReview(String reviewId, String propertyId, String reviewerId, int rating, String comment) {
+        super(reviewId, propertyId, reviewerId, rating, comment);
+    }
 
 }
