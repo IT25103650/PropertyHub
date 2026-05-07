1f9156eb57b6ebc667733cbbaf269dd5f0d2951b
IT25103685 <it25103685@my.sliit.lk>
2026-05-07 07:45:40 +0530

    add constructor to VerifiedReview

--- src/Feedback & Review Management/VerifiedReview.java
+++ src/Feedback & Review Management/VerifiedReview.java
@@ -2,4 +2,9 @@ public class VerifiedReview extends Review {
 
     private String purchaseDate;
 
+    public VerifiedReview(String reviewId, String propertyId, String reviewerId, int rating, String comment, String purchaseDate) {
+        super(reviewId, propertyId, reviewerId, rating, comment);
+        this.purchaseDate = purchaseDate;
+    }
+
 }
