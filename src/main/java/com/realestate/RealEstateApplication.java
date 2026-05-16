75f140a47145d913e19d324fe5943231f1c7b814
IT25103650 <it25103650@my.sliit.lk>
2026-05-17 05:12:40 +0530

    add main application entry point

---
+++ src/main/java/com/realestate/RealEstateApplication.java
@@ -0,0 +1,19 @@
+package com.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = {"com.realestate"})
+@EnableJpaRepositories(basePackages = {"com.realestate"})
+@EntityScan(basePackages = {"com.realestate"})
+public class RealEstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateApplication.class, args);
+	}
+
+}
+
