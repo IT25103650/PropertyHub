8acadb742a5c4c818d216c55a76de46fcd1406bc
IT25103685 <it25103685@my.sliit.lk>
2026-05-17 20:26:01 +0530

    create constructor to PublicReview class

--- src/main/java/com/realestate/models/PublicReview.java
+++ src/main/java/com/realestate/models/PublicReview.java
@@ -2,6 +2,8 @@ package com.realestate.models;
 
 public class PublicReview extends Review {
 
-
+    public PublicReview(String reviewId, String propertyId, String reviewerId, int rating, String comment) {
+        super(reviewId, propertyId, reviewerId, rating, comment);
+    }
 
 }
