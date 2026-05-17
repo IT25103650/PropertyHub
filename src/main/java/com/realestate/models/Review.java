e7ee543271834b3bb50de1ab133aa02995b46dda
IT25103685 <it25103685@my.sliit.lk>
2026-05-17 20:16:52 +0530

    create constructors to Review.java

--- src/main/java/com/realestate/models/Review.java
+++ src/main/java/com/realestate/models/Review.java
@@ -7,5 +7,15 @@ public abstract class Review {
     private String reviewerId;
     private int rating;
     private String comment;
-    
+
+    public Review() {}
+
+    public Review(String reviewId, String propertyId, String reviewerId, int rating, String comment) {
+        this.reviewId = reviewId;
+        this.propertyId = propertyId;
+        this.reviewerId = reviewerId;
+        this.rating = rating;
+        this.comment = comment;
+    }
+
 }
