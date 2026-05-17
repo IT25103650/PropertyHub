2d67cf082f2ae370dbd75adf1ae962aedfc1373c
IT25103685 <it25103685@my.sliit.lk>
2026-05-17 20:34:35 +0530

    create constructor to VerifiedReview class

--- src/main/java/com/realestate/models/VerifiedReview.java
+++ src/main/java/com/realestate/models/VerifiedReview.java
@@ -4,5 +4,9 @@ public class VerifiedReview extends Review{
 
     private String purchaseDate;
 
+    public VerifiedReview(String reviewId, String propertyId, String reviewerId, int rating, String comment, String purchaseDate) {
+        super(reviewId, propertyId, reviewerId, rating, comment);
+        this.purchaseDate = purchaseDate;
+    }
 
 }
