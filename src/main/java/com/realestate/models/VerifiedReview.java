b003b51cd96d7c814ba959f1c34f35f65ad1e189
IT25103685 <it25103685@my.sliit.lk>
2026-05-17 20:45:27 +0530

    add displayReview( ) to VerifiedReview class

--- src/main/java/com/realestate/models/VerifiedReview.java
+++ src/main/java/com/realestate/models/VerifiedReview.java
@@ -12,5 +12,9 @@ public class VerifiedReview extends Review{
     public String getPurchaseDate() { return purchaseDate; }
     public void setPurchaseDate(String purchaseDate) { this.purchaseDate = purchaseDate; }
 
+    @Override
+    public String displayReview() {
+        return "Verified Buyer Review (Purchased: " + purchaseDate + ") [" + getRating() + "/5]: " + getComment() + " ✓";
+    }
 
 }
