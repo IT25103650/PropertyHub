fb40d4a1a0e8afedac2cc4e038db6115fdc926fe
IT25103685 <it25103685@my.sliit.lk>
2026-05-17 20:27:06 +0530

    override displayReview( ) method in PublicReview

--- src/main/java/com/realestate/models/PublicReview.java
+++ src/main/java/com/realestate/models/PublicReview.java
@@ -6,4 +6,9 @@ public class PublicReview extends Review {
         super(reviewId, propertyId, reviewerId, rating, comment);
     }
 
+    @Override
+    public String displayReview() {
+        return "Public Review [" + getRating() + "/5]: " + getComment();
+    }
+
 }
