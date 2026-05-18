24ff1b2d76b75bd0686d4ec9b9f4258954128399
IT25101236 <it25101236@my.sliit.lk>
2026-05-18 11:56:19 +0530

    Add getter for  booking ID, property ID, buyer ID, booking date , status

--- src/main/java/com/realestate/models/Booking.java
+++ src/main/java/com/realestate/models/Booking.java
@@ -16,3 +16,18 @@ public abstract class Booking {
         this.bookingDate = bookingDate;
         this.status = status;
     }
+	
+	public String getBookingId() { return bookingId; }
+    public void setBookingId(String bookingId) { this.bookingId = bookingId; }
+
+    public String getBuyerId() { return buyerId; }
+    public void setBuyerId(String buyerId) { this.buyerId = buyerId; }
+
+    public String getPropertyId() { return propertyId; }
+    public void setPropertyId(String propertyId) { this.propertyId = propertyId; }
+
+    public String getBookingDate() { return bookingDate; }
+    public void setBookingDate(String bookingDate) { this.bookingDate = bookingDate; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
