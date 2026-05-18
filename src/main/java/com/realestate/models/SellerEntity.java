986d8d7998b299b197e4fc1208a1e4e5217e5bc0
IT25102330 <IT25102330@my.sliit.lk>
2026-05-18 11:05:47 +0530

    Update Some Changes In Constructor

--- src/main/java/com/realestate/models/SellerEntity.java
+++ src/main/java/com/realestate/models/SellerEntity.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 
 public class SellerEntity {
 
+    // Implement Constructor
     public SellerEntity(String firstName, String lastName, String email, String passwordHash, String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
